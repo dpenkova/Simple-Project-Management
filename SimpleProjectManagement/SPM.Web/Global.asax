8e501e1522f679136a38add01a0e69aa4cbf00d3
dpenkova <dpenkova@gmail.com>
2014-11-10 13:04:43 +0200

    Create solution

---
+++ SimpleProjectManagement/SPM.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SPM.Web.MvcApplication" Language="C#" %>
---
+++ SimpleProjectManagement/SPM.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SPM.Web.Startup))]
+namespace SPM.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
