34f50890560802abdfec75b73783286a69ca8d85
dpenkova <dpenkova@gmail.com>
2014-11-12 22:35:52 +0200

    Repository implemented

---
+++ SimpleProjectManagement/SPM.Data/Repositories/DeletableEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace SPM.Data.Repositories
+{
+    using System.Linq;
+    using System.Data.Entity;
+
+    using SPM.Data.Contracts.Repository;
+    using SPM.Data.Contracts.Models;
+
+    public class DeletableEntityRepository<T> : GenericRepository<T>, IDeletableEntityRepository<T>
+        where T : class, IDeletableEntity
+    {
+        public DeletableEntityRepository(DbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<T> All()
+        {
+            return base.All().Where(x => !x.IsDeleted);
+        }
+
+        public IQueryable<T> AllWithDeleted()
+        {
+            return base.All();
+        }
+    }
+}
