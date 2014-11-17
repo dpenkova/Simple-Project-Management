c140d1fa13cea303b82f6245de2ced3a34da039b
dpenkova <dpenkova@gmail.com>
2014-11-18 00:35:48 +0200

    Fixed Login bugs, changed login to use username instead of e-mail

--- SimpleProjectManagement/SPM.Data/Migrations/SeedData.cs
+++ SimpleProjectManagement/SPM.Data/Migrations/SeedData.cs
@@ -1,13 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using SPM.Models;
-
-namespace SPM.Data.Migrations
+﻿namespace SPM.Data.Migrations
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using SPM.Models;
+
     public class SeedData
     {
         public static Random Rand = new Random();
